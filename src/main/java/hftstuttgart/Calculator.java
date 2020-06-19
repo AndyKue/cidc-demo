a24214dbddb8215880abad043003f7d6b8e22dab
AndyKue <71kuan1bif@hft-stuttgart.de>
2020-06-19 17:57:22 +0200

    change

--- src/main/java/hftstuttgart/Calculator.java
+++ src/main/java/hftstuttgart/Calculator.java
@@ -4,14 +4,14 @@ package hftstuttgart;
 public class Calculator {
     
     public int sum(int op1, int op2) {
-        int sum = 0;
+        int sum = 1;
         sum += op1;
         sum += op2;
         return sum;
     }    
 
     public int prod(int op1, int op2) {
-        int sum = 0;
+        int sum = 1;
         sum *= op1;
         sum *= op2;
         return sum;
